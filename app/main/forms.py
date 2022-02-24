0f0fd9e062e0abb11653ce07362dab2ba14c5be2
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-02-24 15:25:31 +0300

    Deployment to Heroku

---
+++ app/main/forms.py
@@ -0,0 +1,12 @@
+from turtle import title
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+class ReviewForm(FlaskForm):
+    
+    title = StringField("Review title", validators = [DataRequired()])
+    
+    review = TextAreaField('Movie review', validators=[DataRequired()])
+    
+    submit = SubmitField('Submit')
